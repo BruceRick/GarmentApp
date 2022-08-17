06b1c9d420415731f10631935d6e9f0815ee9817
Charlie Rick <rick0048@algonquinlive.com>
2022-08-17 16:05:37 -0400

    Initial Commit

---
+++ GarmentApp/GarmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarmentAppApp.swift
+//  GarmentApp
+//
+//  Created by Charlie Rick on 2022-08-17.
+//
+
+import SwiftUI
+
+@main
+struct GarmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
