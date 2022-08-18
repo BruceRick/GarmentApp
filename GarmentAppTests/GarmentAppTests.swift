63c7326ff4ac3b9e350a36f6827b50a6a9e36eab
Charlie Rick <rick0048@algonquinlive.com>
2022-08-18 16:50:35 -0400

    First attempt at some tests

--- GarmentAppTests/GarmentAppTests.swift
+++ GarmentAppTests/GarmentAppTests.swift
@@ -1,29 +1,46 @@
+import ComposableArchitecture
 import XCTest
+
 @testable import GarmentApp
 
-class GarmentAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+@MainActor
+final class GarmentAppTests: XCTestCase {
+    let mainQueue = DispatchQueue.test
+    
+    func testListAdd() async {
+        let testGarments = [
+            Garment(name: "alpha", creationDate: Date()),
+        ]
+        Storage.set(testGarments, key: .Garments)
+        
+        let store = TestStore(
+            initialState: ListState(),
+            reducer: listReducer,
+            environment: .init()
+        )
+        
+        await store.send(.navigate(.Add)) {
+            $0.navigation = .Add
+        }
+        
+        await store.send(.add(.save)) {
+            $0.navigation = .None
+        }
+        
+        await store.receive(.fetchGarments) {
+            $0.garments = testGarments
+        }
+        
+        await store.receive(.sortGarments) {
+            $0.garments = testGarments
         }
     }
+    
+    func testListDetail() async {
 
+    }
+    
+    func testListSort() async {
+
+    }
 }
